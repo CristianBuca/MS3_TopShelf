bc820992e2e1939d742f2c83c19f152281139caa
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2022-01-12 15:29:17 +0000

    Add(app): Login, register forms backend validation

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 from flask import (
     Flask, flash, render_template, 
-    redirect, request, session, url_for)
+    redirect, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from forms import RegistrationForm, LoginForm
 if os.path.exists("env.py"):
     import env
 
-
+# Credit for app initialization to Code Institute Mini Project Lessons
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -27,32 +28,45 @@ def get_items():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST":
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created {form.username.data}. Welcome aboard!', 'light-green accent-4')
+        return redirect(url_for('get_items'))
 
-        register = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.users.insert_one(register)
-    return render_template("register.html")
+
+    # if request.method == "POST":
+
+    #     register = {
+    #         "username": request.form.get("username").lower(),
+    #         "password": generate_password_hash(request.form.get("password"))
+    #     }
+    #     mongo.db.users.insert_one(register)
+    return render_template("register.html", title='Register', form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        user_exists = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
-
-        if user_exists:
-            if check_password_hash(
-                user_exists["password"], request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
-            else:
-                return redirect(url_for("login"))
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'admin@topshelf.com' and form.password.data == 'password':       
+            flash(f'Welcome {form.username.data}. This is your Top Shelf',  'light-green accent-4')
+            return redirect(url_for('get_items'))
         else:
-            return redirect(url_for("get_items"))
-    return render_template("login.html")
+            flash(f'Login Unsuccessful', 'red accent-4')
+    # if request.method == "POST":
+    #     user_exists = mongo.db.users.find_one(
+    #         {"username": request.form.get("username").lower()})
+
+    #     if user_exists:
+    #         if check_password_hash(
+    #             user_exists["password"], request.form.get("password")):
+    #             session["user"] = request.form.get("username").lower()
+    #         else:
+    #             return redirect(url_for("login"))
+
+    #     else:
+    #         return redirect(url_for("get_items"))
+    return render_template("login.html", title='Login', form=form)
 
 
 if __name__ == "__main__":
