093a920c6d376a2ff82ed82448149a4321ea40bb
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2022-01-22 01:41:12 +0000

    Add(script): Ajax check for 404 img & replace src

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,29 @@
-
-$(document).ready(function(){
-    $('.sidenav').sidenav({
-		edge: "right",
-		closeOnClick: true,
-	});
-    $('.collapsible').collapsible();
-    $('.fixed-action-btn').floatingActionButton();
-    $('.modal').modal();
+// MaterializeCSS initializations
+$('.sidenav').sidenav({
+    edge: "right",
+    closeOnClick: true,
 });
+$('.collapsible').collapsible();
+$('.fixed-action-btn').floatingActionButton();
+$('.modal').modal();
 
-$('img').on('error', function() {
-    $(this).attr('src', '/static/images/avatar_default.jpg');
-})
+// Credit for use of Ajax to replace broken images to my mentor
+// Mo Shami and https://www.sitepoint.com/jquery-replace-broken-images/
+
+$("img").each( function () 
+{
+	var _this = $(this);
+	
+	$.ajax({
+		url:$(this).attr('src'),
+		type:'HEAD',
+		async: false,
+		error:
+			function(e)
+			{
+				if (e.status == '404') {
+					$(_this).attr('src', '/static/images/avatar_default.jpg');
+				}
+			}
+	});
+});
