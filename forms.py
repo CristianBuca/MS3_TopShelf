f0b7e71e5d230303eae2645107b6c4c30207fbb2
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2022-01-26 00:05:03 +0000

    Reformat(forms): Refactor formatting for pep8

--- forms.py
+++ forms.py
@@ -12,28 +12,38 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
 '''
 Registration Form is stored as a class and each input validation requirement is
 defined using the wtforms validators. Validators check the inputs once
-form .validate_on_submit() is called. Errors raised based on these validators 
+form.validate_on_submit() is called. Errors raised based on these validators
 can be picked up by the JQuery form validation plugin on client side.
 '''
+
+
 class RegistrationForm(FlaskForm):
     username = StringField(
         'Username - Only letters and numbers',
-            validators=[DataRequired(), Length(min=3, max=15),
-            # Use Regex to check that username does not contain special characters
-            Regexp('^(?=.*[a-z])\w{3,}$',
-            message='No special characters allowed'),
-    ])
+        validators=[
+            DataRequired(), Length(min=3, max=15),
+            # Use Regex to check that username does not contain special chars
+            Regexp(
+                '^(?=.*[a-z])\w{3,}$',
+                message='No special characters allowed'
+            ),
+        ]
+    )
     email = StringField(
         # Use Email validator to check the email format is valid
         'Email', validators=[DataRequired(), Email()]
     )
     password = PasswordField(
         'Password - Must include at least one uppercase letter and one number',
-            validators=[DataRequired(), Length(min=5, max=20),
-            # Use Regex to check that password includes 1 uppercase letter and 1 number
-            Regexp('^(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,}).{5,}$',
-            message='Your password does not meet the requirements'),
-    ])
+        validators=[
+            DataRequired(), Length(min=5, max=20),
+            # Use Regex to check that password includes 1 upper letter and 1 nr
+            Regexp(
+                '^(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,}).{5,}$',
+                message='Your password does not meet the requirements'
+            ),
+        ]
+    )
     password_check = PasswordField(
         # Use EqualTo validator to match password inputs
         'Confirm Password', validators=[DataRequired(), EqualTo('password')]
@@ -44,9 +54,11 @@ class RegistrationForm(FlaskForm):
 '''
 Registration Form is stored as a class and each input validation requirement is
 defined using the wtforms validators. Validators check the inputs once
-form .validate_on_submit() is called. Errors raised based on these validators 
+form.validate_on_submit() is called. Errors raised based on these validators
 can be picked up by the JQuery form validation plugin on client side.
 '''
+
+
 class LoginForm(FlaskForm):
     username = StringField(
         'Username', validators=[DataRequired(), Length(min=3, max=15)]
@@ -61,9 +73,11 @@ class LoginForm(FlaskForm):
 '''
 Registration Form is stored as a class and each input validation requirement is
 defined using the wtforms validators. Validators check the inputs once
-form .validate_on_submit() is called. Errors raised based on these validators 
+form .validate_on_submit() is called. Errors raised based on these validators
 can be picked up by the JQuery form validation plugin on client side.
 '''
+
+
 class AddStock(FlaskForm):
     name = StringField(
         'Name', validators=[DataRequired(), Length(min=5, max=50)]
@@ -75,9 +89,9 @@ class AddStock(FlaskForm):
     region = SelectField('Region', choices=(
         ('Campbeltown', 'Campbeltown'),
         ('Highland', 'Highland'),
-        ('Islay','Islay'),
+        ('Islay', 'Islay'),
         ('Lowland', 'Lowland'),
-        ('Speyside', 'Speyside')  
+        ('Speyside', 'Speyside')
     ))
     notes = StringField('Additional Notes')
     image = StringField('Image Link')
