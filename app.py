c50c66775fff46e946a6db999d8fc949f5f269e0
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2022-01-26 00:32:08 +0000

    Reformat(app): Refactor format 4 pep8 validation

--- app.py
+++ app.py
@@ -36,7 +36,9 @@ def get_items():
 def my_shelf():
     user = session['user']
     my_shelf = mongo.db.items.find({'owned_by': user})
-    return render_template('my_shelf.html', title='My Shelf', my_shelf=my_shelf)
+    return render_template(
+        'my_shelf.html', title='My Shelf', my_shelf=my_shelf
+    )
 
 
 @app.route('/superuser', methods=['GET', 'POST'])
@@ -46,15 +48,21 @@ def superuser():
         if user['superuser']:
             items = mongo.db.items.find()
             users = mongo.db.users.find()
-            return render_template('superuser.html', title='Admin', users=users, items=items)
+            return render_template(
+                'superuser.html', title='Admin', users=users, items=items
+            )
         else:
-            flash(f'You do not have the required permission to access this feature', 'deep-orange darken-4 yellow-text text-lighten-5')
+            flash(
+                f'You do not meet the requirements to access this feature',
+                'deep-orange darken-4 yellow-text text-lighten-5'
+            )
             return redirect(url_for('my_shelf'))
 
 
 '''
 USER AUTHENTICATION ROUTES
 '''
+
 
 # Register Route
 @app.route('/register', methods=['GET', 'POST'])
@@ -67,35 +75,47 @@ def register():
     If valid
     :return redirect to get_items
     If invalid
-    :return render_template of register.html 
+    :return render_template of register.html
     '''
     form = RegistrationForm()
     if request.method == 'POST':
-        user_exists = mongo.db.users.find_one({'username': form.username.data.lower()})
+        user_exists = mongo.db.users.find_one({
+            'username': form.username.data.lower()
+        })
         email_exists = mongo.db.users.find_one({'email': form.email.data})
         if user_exists:
-            flash('Username already taken', 'deep-orange darken-4 yellow-text text-lighten-5')
+            flash(
+                'Username already taken',
+                'deep-orange darken-4 yellow-text text-lighten-5'
+            )
             return redirect(url_for('register'))
         elif email_exists:
-            flash('Email already in use!', 'deep-orange darken-4 yellow-text text-lighten-5')
+            flash(
+                'Email already in use!',
+                'deep-orange darken-4 yellow-text text-lighten-5'
+            )
         elif form.validate_on_submit():
             register = {
                 'username': form.username.data.lower(),
-                # Password is hashed using werkzeug.security before being added to DB
+                # Password is hashed with werkzeug.security before adding to DB
                 'password': generate_password_hash(form.password.data),
                 'email': form.email.data,
-                # User is assigned default avatar that he can later change from profile page
+                # User is assigned default avatar
                 'avatar': '/static/images/avatar_default.jpg',
-                # Superuser status set false by default. Only DB admin can promote users
+                # Superuser status set false by default
                 'superuser': False
             }
-            # If form passes field validation then new account data is inserted in the DB
+            # If form passes validation then new data is inserted in the DB
             mongo.db.users.insert_one(register)
             # User is added to session cookies and redirected to landing page
             session['user'] = form.username.data.lower()
-            flash(f'Account created {form.username.data}. Welcome aboard!', 'light-green darken-3 yellow-text text-lighten-5')
+            flash(
+                f'Account created {form.username.data}. Welcome aboard!',
+                'light-green darken-3 yellow-text text-lighten-5'
+            )
             return redirect(url_for('get_items'))
     return render_template('register.html', title='Register', form=form)
+
 
 # Login Route
 @app.route('/login', methods=['GET', 'POST'])
@@ -110,17 +130,27 @@ def login():
     '''
     form = LoginForm()
     if request.method == 'POST':
-        user_exists = mongo.db.users.find_one({"username": form.username.data.lower()})
+        user_exists = mongo.db.users.find_one({
+            "username": form.username.data.lower()
+        })
         # Use check_password_hash from werkzeug.security to check password
-        if user_exists and check_password_hash(user_exists['password'], form.password.data):
+        if user_exists and check_password_hash(
+                user_exists['password'], form.password.data):
             # Add user to session cookies
             session['user'] = form.username.data.lower()
-            flash(f'Welcome {form.username.data}. This is what your shelf looks like',  'light-green darken-3 yellow-text text-lighten-5')
+            flash(
+                f'Welcome {form.username.data}. This is your Shelf',
+                'light-green darken-3 yellow-text text-lighten-5'
+            )
             return redirect(url_for('my_shelf', username=session['user']))
         else:
-            flash(f'Login Unsuccessful. Double check your username and password!', 'deep-orange darken-4 yellow-text text-lighten-5')
+            flash(
+                f'Login Unsuccessful. Check your username and password!',
+                'deep-orange darken-4 yellow-text text-lighten-5'
+            )
             return redirect(url_for('login'))
     return render_template('login.html', title='Login', form=form)
+
 
 # Logout Route
 @app.route('/logout')
@@ -130,7 +160,10 @@ def logout():
     Displays flash message to confirm logout,
     :return redirect to login
     '''
-    flash(f'You have been logged out', 'light-green darken-3 yellow-text text-lighten-5')
+    flash(
+        f'You have been logged out',
+        'light-green darken-3 yellow-text text-lighten-5'
+    )
     session.pop('user')
     return redirect(url_for('login'))
 
@@ -153,7 +186,7 @@ def search_items():
     return render_template('items.html', title='Search Results', items=items)
 
 
-#Super Search Route for users and items in DB
+# Super Search Route for users and items in DB
 @app.route('/super_search', methods=['GET', 'POST'])
 def super_search():
     '''
@@ -164,10 +197,12 @@ def super_search():
     super_search = request.form.get('super_search')
     users = mongo.db.users.find({'$text': {'$search': super_search}})
     items = mongo.db.items.find({'$text': {'$search': super_search}})
-    return render_template('superuser.html', Title='Search Results', users=users, items=items)
+    return render_template(
+        'superuser.html', Title='Search Results', users=users, items=items
+    )
 
 
-#Add Stock Route
+# Add Stock Route
 @app.route('/add_stock', methods=['GET', 'POST'])
 def add_stock():
     '''
@@ -184,18 +219,21 @@ def add_stock():
         if form.validate_on_submit():
             # Store form data in stock dictionary
             stock = {
-                'item_name' : form.name.data,
-                'region_name' : form.region.data,
-                'age' : form.age.data,
-                'distillery' : form.distillery.data,
-                'notes' : form.notes.data,
-                'image' : form.image.data,
-                'share' : share,
+                'item_name': form.name.data,
+                'region_name': form.region.data,
+                'age': form.age.data,
+                'distillery': form.distillery.data,
+                'notes': form.notes.data,
+                'image': form.image.data,
+                'share': share,
                 'owned_by': session['user'].lower()
             }
             # Insert stock dictionary in DB
             mongo.db.items.insert_one(stock)
-            flash(f'New item was added to your shelf', 'light-green darken-3 yellow-text text-lighten-5')
+            flash(
+                f'New item was added to your shelf',
+                'light-green darken-3 yellow-text text-lighten-5'
+            )
             return redirect(url_for('get_items'))
     return render_template('add_stock.html', title='Add Stock', form=form)
 
@@ -235,9 +273,16 @@ def profile(username):
                 }
                 # replace user data based on user_id
                 mongo.db.users.replace_one({'_id': ObjectId(user_id)}, update)
-                flash(f'Profile successfully updated', 'light-green darken-3 yellow-text text-lighten-5')
-                return render_template('profile.html', title='Profile', user=user, form=form)
-        return render_template('profile.html', title='Profile', user=user, form=form)
+                flash(
+                    f'Profile successfully updated',
+                    'light-green darken-3 yellow-text text-lighten-5'
+                )
+                return render_template(
+                    'profile.html', title='Profile', user=user, form=form
+                )
+        return render_template(
+            'profile.html', title='Profile', user=user, form=form
+        )
 
 
 # Change stock route
@@ -262,18 +307,21 @@ def change_stock(item_id):
         if form.validate_on_submit():
             # Store new form data in stock dictionary
             stock = {
-                'item_name' : form.name.data,
-                'region_name' : form.region.data,
-                'age' : form.age.data,
-                'distillery' : form.distillery.data,
-                'notes' : form.notes.data,
-                'image' : form.image.data,
-                'share' : share,
+                'item_name': form.name.data,
+                'region_name': form.region.data,
+                'age': form.age.data,
+                'distillery': form.distillery.data,
+                'notes': form.notes.data,
+                'image': form.image.data,
+                'share': share,
                 'owned_by': session['user'].lower()
             }
             # replace item data in DB with data from stock dictionary
             mongo.db.items.replace_one({'_id': ObjectId(item_id)}, stock)
-            flash(f'Your Shelf has been updated', 'light-green darken-3 yellow-text text-lighten-5')
+            flash(
+                f'Your Shelf has been updated',
+                'light-green darken-3 yellow-text text-lighten-5'
+            )
             return redirect(url_for('my_shelf'))
     elif request.method == 'GET':
         form.name.data = item['item_name']
@@ -282,9 +330,12 @@ def change_stock(item_id):
         form.region.data = item['region_name']
         form.notes.data = item['notes']
         form.image.data = item['image']
-    return render_template('change_stock.html', title='Change Stock', item=item, form=form)
+    return render_template(
+        'change_stock.html', title='Change Stock', item=item, form=form
+    )
 
-#Remove Stock route
+
+# Remove Stock route
 @app.route('/remove_stock/<item_id>')
 def remove_stock(item_id):
     '''
@@ -293,10 +344,14 @@ def remove_stock(item_id):
     :return redirect to my_shelf
     '''
     mongo.db.items.delete_one({'_id': ObjectId(item_id)})
-    flash(f'Item Removed from Shelf', 'light-green darken-3 yellow-text text-lighten-5')
-    return  redirect(url_for('my_shelf'))
+    flash(
+        f'Item Removed from Shelf',
+        'light-green darken-3 yellow-text text-lighten-5'
+    )
+    return redirect(url_for('my_shelf'))
 
-#Remove User route
+
+# Remove User route
 @app.route('/remove_user/<user_id>')
 def remove_user(user_id):
     '''
@@ -306,7 +361,10 @@ def remove_user(user_id):
     :return redirect to superuser
     '''
     mongo.db.users.delete_one({'_id': ObjectId(user_id)})
-    flash(f'User removed from database', 'light-green darken-3 yellow-text text-lighten-5')
+    flash(
+        f'User removed from database',
+        'light-green darken-3 yellow-text text-lighten-5'
+    )
     return redirect(url_for('superuser'))
 
 
@@ -314,38 +372,43 @@ def remove_user(user_id):
 ERROR HANDLING
 '''
 
+
 # Error 400
 @app.errorhandler(400)
 def bad_request(e: object) -> object:
-    return render_template('errors/error_400.html', title='400 Error', error=e), 400
+    return render_template(
+        'errors/error_400.html', title='400 Error', error=e), 400
 
 
 # Error 401
 @app.errorhandler(401)
 def unauthorized(e: object) -> object:
-    return render_template('errors/error_401.html', title='401 Error', error=e), 401
+    return render_template(
+        'errors/error_401.html', title='401 Error', error=e), 401
 
 
 # Error 404
 @app.errorhandler(404)
 def page_not_found(e: object) -> object:
-    return render_template('errors/error_404.html', title='404 Error', error=e), 404
+    return render_template(
+        'errors/error_404.html', title='404 Error', error=e), 404
 
 
 # Error 405
 @app.errorhandler(405)
 def method_not_allowed(e: object) -> object:
-    return render_template('errors/error_405.html', title='405 Error', error=e), 405
+    return render_template(
+        'errors/error_405.html', title='405 Error', error=e), 405
 
 
 # Error 500
 @app.errorhandler(500)
 def internal_server_error(e: object) -> object:
-    return render_template('errors/error_500.html', title='500 Error', error=e), 500
+    return render_template(
+        'errors/error_500.html', title='500 Error', error=e), 500
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=False)
-
